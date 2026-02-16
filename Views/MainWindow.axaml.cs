933c45d2091bd1be73bc1d66e625844782a6ed96
Piesio0231 <dino.rambo2010@gmail.com>
2026-02-16 17:53:16 +0100

    Add project files.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Android.Bluetooth;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Java.Util;
+
+namespace AvaloniaApplication9.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+   
+
+}
